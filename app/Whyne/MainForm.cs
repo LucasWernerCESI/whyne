fa61c9f0c0f1a08dd51a14b18b11e7a8dd53c815
lucas <lucas@whyne>
2021-10-21 20:10:24 +0200

    [Init] Global Architecture + Tests

---
+++ app/Whyne/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Whyne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            ApiClient client = new ApiClient("https://localhost:5001/");
+            Account account = await client.Get<Account>("account");
+            List<Account> accounts = new List<Account>();
+            accounts.Add(account);
+            dataGridView1.DataSource = accounts;
+            AccountForm accountForm = new AccountForm();
+            accountForm.Show();
+        }
+    }
+
+}
