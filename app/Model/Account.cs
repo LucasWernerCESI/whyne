799923893b3555ebef8c2a28c2cae940a24609f1
Lucas Werner <lucas@werner.wtf>
2021-12-10 17:26:41 +0100

    [APP][Mapping] Properly mapped Account properties

---
+++ app/Model/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace App.Model
+{
+    public class Account
+    {
+        [JsonPropertyName("accountId")] public int AccountId { get; set; }
+
+        [JsonPropertyName("title")] public string Title { get; set; }
+
+        [JsonPropertyName("firstName")] public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")] public string LastName { get; set; }
+
+        [JsonPropertyName("email")] public string Email { get; set; }
+
+        [JsonPropertyName("role")] public string Role { get; set; }
+
+        [JsonPropertyName("createdAt")] public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyName("updatedAt")] public DateTime? UpdatedAt { get; set; }
+    }
+}
