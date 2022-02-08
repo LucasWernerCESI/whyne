9a2872d61d500381e6d60b8127c21b1fea00a4d7
ombri <ombrinvis@gmail.com>
2022-02-08 10:17:44 +0100

    code tempo test et com

--- app/SearchFormV2.cs
+++ app/SearchFormV2.cs
@@ -14,10 +14,15 @@ namespace Whyne.App
     public partial class SearchFormV2 : Form
     {
         private readonly ItemService _ItemS;
+        private readonly ProviderService _ProviderS;
         public SearchFormV2()
         {
             InitializeComponent();
-            List<string> famillies = new List<string>();
+
+            var fam = _ItemS.GetEveryFamilly;           //
+            var prov = _ProviderS.GetProvider;          // tempo
+
+            List<string> famillies = new List<string>();    //liste à remplir
             foreach (string familly in famillies)
             {
                 ToolStripTextBox FamMenubt = new ToolStripTextBox();
@@ -25,7 +30,7 @@ namespace Whyne.App
 
                 Filterbox2.DropDownItems.Add(FamMenubt);
             }
-            List<string> providers = new List<string>();
+            List<string> providers = new List<string>();    //liste à remplir
             foreach (string provider in providers)
             {
                 ToolStripTextBox provMenubt = new ToolStripTextBox();
@@ -33,17 +38,49 @@ namespace Whyne.App
 
                 Filterbox3.DropDownItems.Add(provMenubt);
             }
-
         }
 
-
-        private void Filterbox2_Click(object sender, EventArgs e)    //using FamillyFilter
+        DataTable Tablesource;                      //tempo
+        private void starttable()                   //  
         {
+            Tablesource = new DataTable();          //
+            dataGridView1.DataSource = Tablesource; //
+        }                                           //
 
+
+
+        private void Filterbox2_Click(object sender, EventArgs e)    //using FamillyFilter //les filtres ne s'aditionnent pas, il s'écrasent entre eux
+        {
+            var memo = _ItemS.FamillyFilter;
+            if (Filterbox3.Text == null)
+            {
+                DataView dv2 = Tablesource.DefaultView;
+                dv2.RowFilter = Filterbox2.Text;
+                dataGridView1.DataSource = dv2;
+            }
+            else
+            {
+                DataView dv2 = Tablesource.DefaultView;
+                dv2.RowFilter = Filterbox2.Text;
+                dataGridView1.DataSource = dv2;
+            }
         }
 
-        private void Filterbox3_Click(object sender, EventArgs e)   //using ProviderFilter
+        private void Filterbox3_Click(object sender, EventArgs e)   //using ProviderFilter //les filtres ne s'aditionnent pas, il s'écrasent entre eux
         {
+            var memo = _ItemS.ProviderFilter;
+            if (Filterbox2.Text == null)
+            {
+                DataView dv3 = Tablesource.DefaultView;
+                dv3.RowFilter = Filterbox3.Text;
+                dataGridView1.DataSource = dv3;
+            }
+            else
+            {
+                DataView dv3 = Tablesource.DefaultView;
+                dv3.RowFilter = Filterbox3.Text;
+                dataGridView1.DataSource = dv3;
+            }
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -60,7 +97,7 @@ namespace Whyne.App
                     return;
                 }
             }
-            if (dataGridView1.Columns[e.ColumnIndex].Name == "Edit")
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Edit")    //utilisation de AddItemForm
             {
                 return;
             }
